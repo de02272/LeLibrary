77faaf9dba873e4e9fafa613232a0f011aafd788
Michael Küthe <de02272@gmail.com>
2018-01-12 19:06:37 +0100

    initial

---
+++ LeServer/src/main/java/org/die_fabrik/ledemo/server/ServerService.java
@@ -0,0 +1,10 @@
+package org.die_fabrik.ledemo.server;
+
+import org.die_fabrik.lelib.server.LeServerService;
+
+/**
+ * Created by Michael on 12.01.2018.
+ */
+
+public class ServerService extends LeServerService {
+}
